2461bcb305694e9671e7da4db28c329b73b6524e
pkkwilliam <pkkwilliam919@gmail.com>
2021-07-09 13:50:53 +0800

    add footer just for now

--- src/components/Footer/FooterErp.js
+++ src/components/Footer/FooterErp.js
@@ -19,27 +19,70 @@ class FooterErp extends Component {
                       data-wow-delay="0.2s"
                     >
                       <a href="index.html" className="f-logo">
-                        <img src={require("../../img/logo2.png")} alt="" />
+                        {/* <img src={require("../../img/logo2.png")} alt="" /> */}
+                        <h3>源代碼科技</h3>
+                        <h3>Bitcode Technology</h3>
                       </a>
                       <p>
                         © 2021 Bitcode Technology Ltd. All rights reserved.
                         Proudly made in Macau
                       </p>
                       <div className="f_social_icon">
-                        {FooterData.socialIcon.map((item) => {
+                        {/* {FooterData.socialIcon.map((item) => {
                           return (
                             <a href="/" key={item.id}>
                               <i className={item.icon}></i>
                             </a>
                           );
-                        })}
+                        })} */}
                       </div>
                     </div>
                   </div>
                 </Reveal>
               );
             })}
-            {FooterData.AboutWidget.map((widget) => {
+            <Reveal effect="fadeInLeft" duration={500}>
+              <div className="col-lg-3 col-sm-6">
+                <div className="f_widget dark_widget about-widget pl_70">
+                  <h3 className="f-title f_500 t_color f_size_18 mb_40">
+                    產品
+                  </h3>
+                  <ul className="list-unstyled f_list">
+                    <li>
+                      <Link>ERP</Link>
+                    </li>
+                    <li>
+                      <Link>SCM</Link>
+                    </li>
+                    <li>
+                      <Link>HCM</Link>
+                    </li>
+                    <li>
+                      <Link>RMMS地產業務</Link>
+                    </li>
+                    <li>
+                      <Link>澳提代收 | PickTB.com</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </Reveal>
+            <Reveal effect="fadeInLeft" duration={500}>
+              <div className="col-lg-5 col-sm-6">
+                <div className="f_widget dark_widget about-widget pl_70">
+                  <h3 className="f-title f_500 t_color f_size_18 mb_40">
+                    聯繫我們
+                  </h3>
+                  <ul className="list-unstyled f_list">
+                    <li>地址: 漁翁街188號永好工業大廈7樓F座</li>
+                    <li>電話: +(853) 6353 0392</li>
+                    <li>郵箱: bitcode.mo@gmail.com</li>
+                  </ul>
+                </div>
+              </div>
+            </Reveal>
+
+            {/* {FooterData.AboutWidget.map((widget) => {
               return (
                 <Reveal effect="fadeInLeft" duration={500} key={widget.id}>
                   <div className="col-lg-3 col-sm-6">
@@ -82,7 +125,7 @@ class FooterErp extends Component {
                   </div>
                 </Reveal>
               );
-            })}
+            })} */}
           </div>
         </div>
       </footer>
