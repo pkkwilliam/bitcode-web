e1b6bf8e83ad62edeffc255ceb3d8f3567acbfe5
pkkwilliam <pkkwilliam@me.com>
2023-02-03 23:15:45 -0800

    add back

--- src/Pages/HomeHosting.js
+++ src/Pages/HomeHosting.js
@@ -20,8 +20,7 @@ class HomeHosting extends ApplicationComponent {
     return (
       <div className="body_wrapper">
         <Seo title={title} description={metaDescription} />
-        內部測試使用
-        {/* <CustomNavbar
+        <CustomNavbar
           mClass="menu_four hosting_menu"
           nClass="w_menu"
           slogo="sticky_logo"
@@ -40,7 +39,7 @@ class HomeHosting extends ApplicationComponent {
           Atext="專注於企業管理軟件與服務領域，為亞太地區提供ERP軟件、集團管理軟件、人力資源管理軟件、財務會計管理軟件、供應鏈管理軟件、客戶關係管理軟件、辦公室自動化服務，支撐了亞太區眾多不同規模企業的信息化建設，推動了企業的管理進步。現在，源代碼亦為企業提供雲計算、平台、應用、數據、業務、知識、信息服務多態融合的2.0版智能化企業雲服務，致力於推動企業和公共組織互聯網化、金融化創新發展，服務企業IT產業轉型升級。"
         />
 
-        <HostingActionTwo /> */}
+        <HostingActionTwo />
         <FooterErp />
       </div>
     );
